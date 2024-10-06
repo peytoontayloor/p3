e2eeab21579e2cb0812a256741c0a0093ba0d465
swaharoy <swaharoy3@gmail.com>
2024-10-06 15:03:44 -0500

    Modifed included headers and updated solve

--- src/RTP.cpp
+++ src/RTP.cpp
@@ -9,61 +9,52 @@
 #include <ompl/base/goals/GoalSampleableRegion.h>
 #include <ompl/geometric/PathGeometric.h>
 
-// TODO: Implement RTP as described
 using namespace ompl::base;
 using namespace ompl::geometric;
 
-//solve is supposed to do approximate path!! --> need to implement this 
 
+/* Calculate exact solution path following RTP algorithm.
+    If timeout, calculates approximate path. */
 PlannerStatus RTP::solve(const PlannerTerminationCondition &ptc)
 {
-    //checks to make sure goal and start state defined correctly (i think)
     checkValidity();
 
-    //grabs goal state from pdef
+    // Sets goal state from problem definition
     auto goal_r = std::dynamic_pointer_cast<GoalSampleableRegion>(pdef_->getGoal());
 
-    //do check like in rrt
     if(goal_r == nullptr)
     {
+        // Goal state undefined
         return base::PlannerStatus::UNRECOGNIZED_GOAL_TYPE;
     }
     
-    //state sampler for the state space:
+    // State sampler for the state space
     StateSamplerPtr sampler = si_->allocStateSampler();
 
-    //create a vector to store the valid states: 
-    //std::vector<ScopedState<>> starts;
-    //create parents vector that essentially does parent[i] = k or starts[i]'s parent = k
-    //parents stores the index of the parent of i in starts vector
-    //std::vector<size_t> parents;
-
-    //starts = [start, sample1, sample2]
-    //parents = [nullptr, 0, 1]
-    //indicates parent of start is none (root), parents of sample1 is start, and parent of sample2 is sample1.
-
-    //populate with starts
+    // populate with starts
     while (const State *s = pis_.nextStart())
     {
         if(si_->isValid(s))
         {
-            //inserts state space into vector if it is valid
+            // Inserts valid state space
             starts.push_back(ScopedState<>(si_->getStateSpace(), s));
-            //no parent for start state so doing -1
+
+            // No parent for start state, indicated by -1
             parents.push_back(-1);
         }
     }
 
+
     if (starts.size() == 0)
     {
-        //if no valid start states, return invalid
+        // No valid initial states
         return PlannerStatus::INVALID_START;
     }
 
     //ScopedState<> sample(si_), goal(si_);
     ScopedState<> qb(si_);
 
-    //runs until planner termination condition met
+    // Runs until planner termination condition is met
     while (!ptc)
     {
         //uses rng to select random state
@@ -74,59 +65,61 @@ PlannerStatus RTP::solve(const PlannerTerminationCondition &ptc)
         //from rrt documentation, sample a random state, with small chance of qb being the goal
         /* sample random state (with goal biasing) */
         //if random number less than goal bias and goal state can be sampled, qb is goal state, else qb is sample
-        //setting goal bias to 0.05 but not sure if that's a valid bias
         double goalBias_ = 0.05;
         if (rng_.uniform01() < goalBias_ && goal_r->canSample())
             goal_r->sampleGoal(qb.get());
         else
             sampler->sampleUniform(qb.get());
 
-        //if valid path between qa and qb:
+        // If valid path between qa and qb:
         if(si_->checkMotion(qa.get(), qb.get()))
         {
-            //add new state to list of starts
-            //so that will now sample from all start states including new path
+            // Add new state to starts, future samples can now sample the new path
             starts.push_back(ScopedState<>(si_->getStateSpace(), qb.get()));
-            //the parent of new state qb, is the index of qa in starts
-            //find index of qa and starts with loop (might be easier way?)
-            for(size_t index = 0; index < starts.size(); index++)
-            {
-                if(starts[index] == qa)
-                {
-                    break;
-                }
-            }
-            parents.push_back(index);
 
-            //if goal state was just added to the tree, find path and return:
+            // Assign index of qa in starts to be parent of qb
+            parents.push_back(state_id);
+            
+            // TODO: "can i plus in qb.get()?"
+            //if goal state was just adfed to the tree, find path and return:
             //not sure parameters of isSatisfied, got from RRT but am not sure? can i plus in qb.get()
-           if(goal_r->isSatisfied(qb.get()))
-           {
-               //need to get path and return it 
-               auto path = std::make_shared<PathGeometric>(si_);
-               //set index i to the index of qb in starts
-               //hoping that .back() correctl accesses last element in parents which should correspond to last element in starts, qb
-               size_t i = parents.back();
-               //add qb aka goal to the path:
-               path->append(qb.get());
+            // Check if qb satisfies goal
+            if(goal_r->isSatisfied(qb.get()))
+            {
+                // Instantiate variable to store path
+                auto path = std::make_shared<PathGeometric>(si_);
 
-               //now loop until at start, appending to the path: (-1 corresponds to no parent, aka root)
-               while(i != -1)
-               {
-                   path->append(starts[i].get());
-                   //update index to parent corresponding to next
-                   i = parents[i];
-               }
-               //since did path from goal to start, need to reverse to get correct path
-               path->reverse();
-               return PlannerStatus::EXACT_SOLUTION;
-           }
+                // Add qb (the goal) to path
+                path->append(qb.get());
 
+                //set index i to the index of qb in starts
+                //hoping that .back() correctly accesses last element in parents which should correspond to last element in starts, qb
+                
+                // Index of qb's parent
+                size_t i = parents.back();
+
+                // Construct path until root (parent idx = -1) is found
+                while(i != -1)
+                {
+                    // Add state corresponding to start[i] to path
+                    path->append(starts[i].get());
+
+                    // Update index to parent corresponding to next
+                    i = parents[i];
+                }
+
+                // Reverse path from goal-to-start to start-to-goal
+                path->reverse();
+
+                // TODO: double check "addSolutionPath" is necessary
+                pdef_ ->addSolutionPath(path)
+
+                return PlannerStatus::EXACT_SOLUTION;
+            }
         }
     }
 
-    //if timeout, return approximate path, which ends at the closest state to the goal in the tree
-    //how to find closest state? -->spaceInfo::dist()
+    // Since timeout, find closest state to goal in tree to approx sol path
     double distance = si_->distance(starts[0].get(), goal_r.get());
     size_t index = 0;
     for(size_t i = 1; i < starts.size(); i++)
@@ -139,10 +132,10 @@ PlannerStatus RTP::solve(const PlannerTerminationCondition &ptc)
         }
     }
 
-    //now append path from start to starts[closest to goal]
+    // Construct approx sol path from start to starts[closest to goal]
     auto path = std::make_shared<PathGeometric>(si_);
-    size_t i = parents[index];
     path->append(starts[index]);
+    size_t i = parents[index];
     while(i != -1)
     {
         path->append(starts[i].get());
@@ -150,28 +143,32 @@ PlannerStatus RTP::solve(const PlannerTerminationCondition &ptc)
     }
 
     path->reverse();
+    // TODO: double check "addSolutionPath" is necessary
+    pdef_ ->addSolutionPath(path)
     return PlannerStatus::APPROXIMATE_SOLUTION;
-    //return PlannerStatus::TIMEOUT;
 }
 
-//not sure if can leave as default or if need to add more layers (if so, adjust the header file too)
+
+// TODO: not sure if can leave as default or if need to add more layers (if so, adjust the header file too)
 void RTP::clear() {
 
     //according to rrt, this is called if the input data to the planner has changed and we don't want to continue planning
     Planner::clear();
     sampler.reset();
     freeMemory();
-    //need to clear and free my two global vectors!!
+
+    // Clear global vectors storing tree structure
     starts.clear();
     parents.clear();
 }
 
+// TODO: determine return, currently doing void
 //pdf says it returns a planner data object, but rrt documentation doesnt return anything? 
 //store all the vertexes statically
 //include all of the states and create edges and vertices with them
 //if can find solution from exercise 2 should be okay
 //shared pointer should be automatically released 
-PlannerData RTP::getPlannerData(base::PlannerData &data) const
+void RTP::getPlannerData(base::PlannerData &data) const
 {
     Planner::getPlannerData(data);
 
@@ -182,18 +179,18 @@ PlannerData RTP::getPlannerData(base::PlannerData &data) const
 
     for(size_t i = 0; i < starts.size(); i++)
     {
-        //add start vertex if no parents
+        // Add start vertices
         if(parents[i] == -1)
         {
             data.addStartVertex(base::PlannerDataVertex(starts[i]));
         }
         else
         {
-            //add an edge with the starting with the parent of i and ending with i
+            // Add an edge from parent of i to i
             data.addEdge(base::PlannerDataVertex(starts[parents[i]]), base::PlannerDataVertex(starts[i]));
         }
     }
 
     //not sure if should be returning still?--> pdf says to so should for now
-    return data
+    // return data
 }
