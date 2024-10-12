0fce9862ff9ed1704e85d530e97c88c78bd24b3e
Peyton Elebash <peytayelebash@gmail.com>
2024-10-12 13:18:28 -0500

    added home (no bugs while compiling)

--- src/Project3Exercise3.cpp
+++ src/Project3Exercise3.cpp
@@ -79,7 +79,61 @@ void benchmarkApartment()
 
 void benchmarkHome()
 {
-    // TODO
+    //same as apartment but change path to mesh
+    // Plan in SE3
+    app::SE3RigidBodyPlanning setup;
+
+    // Load robot and environment
+    std::string robot_fname = std::string(OMPLAPP_RESOURCE_DIR) + "/3D/Home_robot.dae";
+    std::string env_fname = std::string(OMPLAPP_RESOURCE_DIR) + "/3D/Home_env.dae";
+    //not sure if needed?
+    //geometric::SimpleSetup ss(space);
+    setup.setRobotMesh(robot_fname);
+    setup.setEnvironmentMesh(env_fname);
+
+    // Define start state
+    base::ScopedState<base::SE3StateSpace> start(setup.getSpaceInformation());
+    start->setX(-4.96);
+    start->setY(-40.62);
+    start->setZ(70.57);
+    start->rotation().setIdentity();
+
+    // define goal state
+    base::ScopedState<base::SE3StateSpace> goal(start);
+    goal->setX(200.49);
+    goal->setY(-40.62);
+    goal->setZ(70.57);
+    goal->rotation().setIdentity();
+  
+    // set the start & goal states
+    setup.setStartAndGoalStates(start, goal);
+  
+    // setting collision checking resolution to 1% of the space extent
+    setup.getSpaceInformation()->setStateValidityCheckingResolution(0.01);
+  
+    // we call setup just so print() can show more information
+    setup.setup();
+    setup.print();
+    
+    // Create benchmark class
+    tools::Benchmark b(setup, "my experiment");
+
+    // Add planners
+    b.addPlanner(base::PlannerPtr(new geometric::RTP(setup.getSpaceInformation())));
+    b.addPlanner(base::PlannerPtr(new geometric::PRM(setup.getSpaceInformation())));
+    b.addPlanner(base::PlannerPtr(new geometric::RRT(setup.getSpaceInformation())));
+    b.addPlanner(base::PlannerPtr(new geometric::EST(setup.getSpaceInformation())));
+
+    // Create benchmark request
+    tools::Benchmark::Request req;
+    req.maxTime = 5.0;
+    req.maxMem = 100.0;
+    req.runCount = 50;
+    req.displayProgress = true;
+    b.benchmark(req);
+
+    // Generate file
+    b.saveResultsToFile();
 }
 
 int main(int /* argc */, char ** /* argv */)
